a4bab21717d0872145e31fe5670e6b999463abda
Gopal Gupta <gopal.gupta@dbrin.com>
2020-02-26 12:37:34 +0530

    add high temperature and low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find coodinates. Try another coordinates!', undefined);
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' Hey, It is currently ' + body.currently.temperature + ' degrees out. High temperature is : ' + body.daily.data[0].temperatureHigh + ' and Low temperature is : ' + body.daily.data[0].temperatureLow + ' . There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
